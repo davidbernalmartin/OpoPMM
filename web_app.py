ff4ca050d3e0afc2bc926fceb5e22c8d9250f7f7
DAVID BERNAL MARTÍN <david.bernal.martin@gmail.com>
2026-03-30 00:21:03 +0200

    iconos

--- web_app.py
+++ web_app.py
@@ -22,10 +22,10 @@ st.set_page_config(
 
 st.markdown(
     f"""
-    <link rel="apple-touch-icon" sizes="180x180" href="assets/logo.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="assets/logo.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="assets/logo.png">
-    """,
+    <link rel="apple-touch-icon" href="assets/logo.png">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    """, 
     unsafe_allow_html=True
 )
 
